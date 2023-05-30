48178967d2d835a838d4a2fae345979b42fd8e23
Logan Wang <logan.wang@glowing.com>
2023-05-30 16:14:27 +0800

    Initial Commit

---
+++ TinyPNG/TinyPNGApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyPNGApp.swift
+//  TinyPNG
+//
+//  Created by Logan Wang on 2023/5/30.
+//
+
+import SwiftUI
+
+@main
+struct TinyPNGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
